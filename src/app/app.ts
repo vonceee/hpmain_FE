a89a30e8217b83fd787ee8c3e36b623579dde3b0
vonceee <alonarvoncedric@gmail.com>
2026-01-06 11:15:34 +0800

    feat: Initialize Angular application with core components, routing, and global styles.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Home } from './pages/home/home';
-import { Industrial } from './pages/solutions/industrial/industrial';
-import { Educational } from './pages/solutions/educational/educational';
-import { aboutUs } from "./pages/company/about-us/about-us";
+import { SideButtons } from './components/side-buttons/side-buttons';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Industrial, Educational, Home, aboutUs],
+  imports: [RouterOutlet, SideButtons],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('hytec_fe');
