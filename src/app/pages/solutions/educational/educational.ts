4f900748618d13e3a1acebee41b41aaa6ddea60a
Albrt2002 <bernardoalbert991@gmail.com>
2026-02-09 09:51:21 +0800

    educational page setup

--- src/app/pages/solutions/educational/educational.ts
+++ src/app/pages/solutions/educational/educational.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { BackToTopComponent } from 'src/app/components/back-to-top/back-to-top';
+import { MainHeader } from 'src/app/components/main-header/main-header';
+import { MainFooter } from 'src/app/components/main-footer/main-footer';  
+
 
 @Component({
   selector: 'educational',
-  imports: [BackToTopComponent],
+  imports: [BackToTopComponent, MainHeader, MainFooter],
   templateUrl: './educational.html',
   styleUrl: './educational.scss',
 })
